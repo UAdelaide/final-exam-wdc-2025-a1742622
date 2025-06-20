cb5f05d49e08de7af6c4465a9d5d155c2b7e0c53
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 07:00:42 +0000

    [automatic commit] 2025-06-20 07:00:42 UTC 121.45.87.120

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -60,12 +60,12 @@ CREATE TABLE WalkRatings (                                              -- store
 
 -- Users:
 INSERT INTO Users (username, email, password_hash, role) VALUES
-('alice123', 'alice@example.com', 'hashed123', 'owner')
-('bobwalker', 'bob@example.com', 'hashed123', 'walker')
-('carol123', 'carol@example.com', 'hashed123', 'owner')
-('jarrydwong', 'jarryd@example.com', 'hashed123', 'walker')
-('veronica123', 'veronica@example.com', 'hashed123', 'owner')
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed123', 'walker'),
+('carol123', 'carol@example.com', 'hashed123', 'owner'),
+('jarrydwong', 'jarryd@example.com', 'hashed123', 'walker'),
+('veronica123', 'veronica@example.com', 'hashed123', 'owner');
 
 -- Dogs:
 INSERT INTO Dogs (owner_id, name, size) VALUES
-((SELECT user_id FROM Users WHERE username = alice123), 'Max', '')
+((SELECT user_id FROM Users WHERE username = alice123), 'Max', 'medium'),
