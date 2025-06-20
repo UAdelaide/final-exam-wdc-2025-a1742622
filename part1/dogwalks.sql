94de84dbac57570a06d7a6bfa27d20bac0eae073
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 06:58:01 +0000

    [automatic commit] 2025-06-20 06:58:01 UTC 121.45.87.120

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -61,3 +61,7 @@ CREATE TABLE WalkRatings (                                              -- store
 -- Users:
 INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner')
+('bobwalker', 'bob@example.com', 'hashed123', 'walker')
+('alice123', 'alice@example.com', 'hashed123', 'owner')
+('alice123', 'alice@example.com', 'hashed123', 'owner')
+('alice123', 'alice@example.com', 'hashed123', 'owner')
