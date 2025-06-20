dffd08b2bfde6ebf7c5ed48f084db451a8c091ba
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 07:39:18 +0000

    [automatic commit] 2025-06-20 07:39:18 UTC 121.45.87.120

--- part1/app.js
+++ part1/app.js
@@ -87,11 +87,13 @@ let db;
         request_id INT NOT NULL,
         walker_id INT NOT NULL,
         owner_id INT NOT NULL,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR (100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        role ENUM('owner', 'walker') NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        rating INT CHECK (rating BETWEEN 1 AND 5),
+        comments TEXT,
+        rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+        FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+        CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
       )
     `);
 
