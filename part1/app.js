b7c97d6a1c9a892daedf73995f19e2eff45aad4c
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 07:41:59 +0000

    [automatic commit] 2025-06-20 07:41:59 UTC 121.45.87.120

--- part1/app.js
+++ part1/app.js
@@ -121,7 +121,7 @@ let db;
         ((SELECT user_id FROM Users WHERE username = 'veronica123'), 'Doggo', 'large')
       `);
 
-// insert walk requests
+      // insert walk requests
       await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
