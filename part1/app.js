c444d20bef17d22aee22362dd4e09705157d4bc1
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 07:25:54 +0000

    [automatic commit] 2025-06-20 07:25:54 UTC 121.45.87.120

--- part1/app.js
+++ part1/app.js
@@ -37,7 +37,8 @@ let db;
     // Create a table if it doesn't exist
     await db.execute(`
       CREATE TABLE IF NOT EXISTS Users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR
         title VARCHAR(255),
         author VARCHAR(255)
       )
