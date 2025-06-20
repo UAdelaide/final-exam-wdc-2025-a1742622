e832c344f7c208393fe4fd7f8df0b25aaf0d2a17
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 07:06:23 +0000

    [automatic commit] 2025-06-20 07:06:23 UTC 121.45.87.120

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -80,4 +80,4 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Pacup'), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Paris'), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Doggo'), '2025-06-10 08:00:00', '60', 'Parklands', 'completed');
+((SELECT dog_id FROM Dogs WHERE name = 'Doggo'), '2025-06-10 08:00:00', '60', 'SomeBeach', 'completed');
