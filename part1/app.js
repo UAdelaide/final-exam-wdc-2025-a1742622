a0494bb890766ec121b7c6b959dd413397cfcf1a
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 07:38:38 +0000

    [automatic commit] 2025-06-20 07:38:38 UTC 121.45.87.120

--- part1/app.js
+++ part1/app.js
@@ -82,8 +82,9 @@ let db;
     `);
 
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS Users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
+      CREATE TABLE IF NOT EXISTS WalkRatings  (
+        rating_id  INT AUTO_INCREMENT PRIMARY KEY,
+        
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR (100) UNIQUE NOT NULL,
         password_hash VARCHAR(255) NOT NULL,
