8503dd84c5df2a1e2ac70b0b282033fec32940bc
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 08:07:47 +0000

    [automatic commit] 2025-06-20 08:07:47 UTC 121.45.87.120

--- part1/app.js
+++ part1/app.js
@@ -76,7 +76,7 @@ let db;
         applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
         FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
-        FOREIGN KEY (walker_id) REFERENCES Users(dog_id),
+        FOREIGN KEY (walker_id) REFERENCES Users(user_id),
         CONSTRAINT unique_application UNIQUE (request_id, walker_id)
       )
     `);
