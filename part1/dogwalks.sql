f61b92d90194e9f165ee3e941e4faee5573b98c6
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-20 05:03:50 +0000

    [automatic commit] 2025-06-20 05:03:50 UTC 121.45.87.120

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS DogWalkService;
 CREATE DATABASE DogWalkService;
 USE DogWalkService;
-CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Users (                                    -- stores all users
+    user_id INT AUTO_INCREMENT PRIMARY KEY,             -- 
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
