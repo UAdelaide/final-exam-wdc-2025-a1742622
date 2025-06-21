af38291e9fcbab7db8ec116b0b05fb0f4f840afa
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-21 05:27:32 +0000

    [automatic commit] 2025-06-21 05:27:32 UTC 121.45.87.120

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -59,7 +59,21 @@ router.post('/:id/apply', async (req, res) => {
   }
 });
 
-// GET all logged-in user's walk requests for the owner dashboard (not realted to a question)\
-
+// GET all logged-in user's walk requests for the owner dashboard (not realted to a question)
+router.get('/my-requests', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
+      FROM WalkRequests wr
+      JOIN Dogs d ON wr.dog_id = d.dog_id
+      JOIN Users u ON d.owner_id = u.user_id
+      WHERE wr.status = 'open'
+    `);
+    res.json(rows);
+  } catch (error) {
+    console.error('SQL Error:', error);
+    res.status(500).json({ error: 'Failed to fetch walk requests' });
+  }
+});
 
 module.exports = router;
