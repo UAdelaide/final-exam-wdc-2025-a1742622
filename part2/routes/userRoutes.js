ad8c407a878c566dac28d153d0015c6060ce2eb6
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-21 02:15:47 +0000

    [automatic commit] 2025-06-21 02:15:47 UTC 121.45.87.120

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -52,7 +52,10 @@ router.post('/login', async (req, res) => {
     // Store user in session
     req.session.user = rows[0];
 
-    res.json({ message: 'Login successful', user: rows[0] });
+    res.json({
+      message: 'Login successful',
+      user: rows[0],
+    });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
