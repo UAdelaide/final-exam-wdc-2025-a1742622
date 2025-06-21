a64c49a344741c5a9fd037f4dfefedac30d9817a
Jarryd Wong <a1742622@adelaide.edu.au>
2025-06-21 02:07:05 +0000

    [automatic commit] 2025-06-21 02:07:05 UTC 121.45.87.120

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Session related Middleware   SECTION
+// Session related Middleware                               [QUESTION 13]
 app.use(session({
     secret: '',
     resave: false,
